62b596723ae4cb9fdf0e1c5a054fd30f7c936f10
youluna <njuzhaoguoyan@foxmail.com>
2019-03-29 11:41:52 +0800

    chore(*): create alifd

--- packages/conventional-changelog-angular/writer-opts.js
+++ packages/conventional-changelog-angular/writer-opts.js
@@ -41,6 +41,8 @@ function getWriterOpts () {
         commit.type = `Performance Improvements`
       } else if (commit.type === `revert`) {
         commit.type = `Reverts`
+      } else if (commit.type === `deprecated`) {
+        commit.type = `Deprecated`;
       } else if (discard) {
         return
       } else if (commit.type === `docs`) {
